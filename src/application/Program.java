78b49c8cc9d09a3bbed248432397b8a378493090
Wanderley Júnior <junior_lima06@hotmail.com>
2020-11-09 23:53:37 -0300

    Exception3

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally Block Executed!");
+		}
+
+	}
+
+}
